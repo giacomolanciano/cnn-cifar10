735d80012f809644cece0f7d382ee80a5b6312d5
Giacomo Lanciano <g.lanciano1@gmail.com>
2018-06-06 20:36:37 +0200

    add stats collection

--- cnn.py
+++ cnn.py
@@ -1,12 +1,20 @@
+import pickle
+from datetime import timedelta
+
 import tensorflow as tf
+import time
+import matplotlib.pyplot as plt
+
 from to_tfrecord import TRAINING_SET_PATH, TEST_SET_PATH
 
 
-EPOCHS = 1000
-BATCH_SIZE = 128
 CIFAR10_TRAIN_SIZE = 50000
 CIFAR10_TEST_SIZE = 10000
+EPOCHS = 1000
+BATCH_SIZE = 128
 KEEP_PROB = 0.5
+ACCURACY_SAMPLING = 50
+MODEL_CHECKPOINT_SAMPLING = 100
 CHECKPOINT_PATH = 'cnn.ckpt'
 
 
@@ -30,6 +38,25 @@ def load_dataset(dataset_filename, batch_size=None, take=None):
     if take:
         dataset = dataset.take(take)
     return dataset
+
+
+def plot_curve(curve, fig_name, fig_ext='.png'):
+    fig = fig_name + fig_ext
+    fig_zoom = fig_name + '_zoom' + fig_ext
+
+    measures_num = len(curve)
+    plt.figure()
+    plt.plot(range(1, measures_num + 1), curve)
+    plt.axis([1, measures_num, 0, 1])
+    plt.savefig(fig, bbox_inches='tight')
+
+    plt.figure()
+    plt.plot(range(1, measures_num + 1), curve)
+    plt.savefig(fig_zoom, bbox_inches='tight')
+
+
+def dump_results(results, results_filename):
+    pickle.dump(results, open(results_filename, 'wb'))
 
 
 def main(argv=None):
@@ -82,9 +109,6 @@ def main(argv=None):
 
     ####################################################################################################################
 
-    # training sessions
-    model_saver = tf.train.Saver()
-    accuracy_curve = []
     with tf.Session() as sess:
         sess.run(tf.global_variables_initializer())
 
@@ -92,21 +116,31 @@ def main(argv=None):
         train_dataset = load_dataset(TRAINING_SET_PATH, batch_size=BATCH_SIZE)
         iterator = train_dataset.make_one_shot_iterator()
 
+        # training sessions
+        model_saver = tf.train.Saver()
+        training_accuracy_curve = []
+        start_time = time.time()
         for epoch in range(EPOCHS):
             # get batch from dataset
             X_batch, y_batch = iterator.get_next()
 
             sess.run(train_step, feed_dict={X: X_batch, y: y_batch, keep_prob: KEEP_PROB})
 
-            if epoch % 100 == 0:
+            if epoch % ACCURACY_SAMPLING == 0 or epoch == EPOCHS - 1:
                 epoch_accuracy = sess.run(accuracy, feed_dict={X: X_batch, y: y_batch, keep_prob: 1.0})
-                accuracy_curve.append(epoch_accuracy)
+                training_accuracy_curve.append(epoch_accuracy)
                 print('accuracy: {}'.format(epoch_accuracy))
 
+            if epoch % MODEL_CHECKPOINT_SAMPLING == 0 or epoch == EPOCHS - 1:
                 # dump current state
                 model_saver.save(sess, CHECKPOINT_PATH, global_step=epoch)
 
-        print('accuracy curve:', accuracy_curve)
+        # collect training results
+        elapsed_time = (time.time() - start_time)
+        training_seconds = timedelta(seconds=elapsed_time)
+        print('training time:', training_seconds, 'seconds')
+        print('training accuracy curve:', training_accuracy_curve)
+        plot_curve(training_accuracy_curve, fig_name='train_accuracy')
 
         # get test split from dataset
         test_dataset = load_dataset(TEST_SET_PATH, batch_size=CIFAR10_TEST_SIZE)
@@ -115,7 +149,14 @@ def main(argv=None):
 
         # validation
         test_accuracy = sess.run(accuracy, feed_dict={X: X_test, y: y_test, keep_prob: 1.0})
-        print('test accuracy: {}'.format(test_accuracy))
+        print('test accuracy:', test_accuracy)
+
+        # store results
+        results = dict()
+        results['training_seconds'] = training_seconds
+        results['training_accuracy_curve'] = training_accuracy_curve
+        results['test_accuracy'] = test_accuracy
+        dump_results(results, 'results.pickle')
 
 
 if __name__ == '__main__':
