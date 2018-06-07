f890613f929d11ad1663327d422cc6aa4a8ae068
Giacomo Lanciano <g.lanciano1@gmail.com>
2018-06-08 00:00:01 +0200

    restructure network and params

--- cnn.py
+++ cnn.py
@@ -12,32 +12,56 @@ from to_tfrecord import TRAINING_SET_PATH, TEST_SET_PATH
 CIFAR10_CLASSES = 10
 CIFAR10_TRAIN_SIZE = 50000
 CIFAR10_TEST_SIZE = 10000
+
 EPOCHS = 1000
-BATCH_SIZE = 150
-MODEL_CHECKPOINT_SAMPLING = 100
-ACCURACY_SAMPLING = 10
-CHECKPOINT_FILENAME = 'cnn.ckpt'
-KEEP_PROB = 0.5
+BATCH_SIZE = 128
+KEEP_PROB_CONV = 0.8
+KEEP_PROB_DENSE = 0.5
 LEARNING_RATE = 0.01
 
-CONV_LAYER_PARAMS = {
-    "filters": 64,
-    "kernel_size": (5, 5),
-    "strides": 1,
-    "activation": tf.nn.relu,
-    "padding": 'same'
+CONV_LAYER_PARAMS_1 = {
+    'filters': 48,
+    'kernel_size': (5, 5),
+    'strides': 1,
+    'activation': tf.nn.relu,
+    'padding': 'same'
+}
+
+CONV_LAYER_PARAMS_2 = {
+    'filters': 96,
+    'kernel_size': (5, 5),
+    'strides': 1,
+    'activation': tf.nn.relu,
+    'padding': 'same'
+}
+
+CONV_LAYER_PARAMS_3 = {
+    'filters': 192,
+    'kernel_size': (5, 5),
+    'strides': 1,
+    'activation': tf.nn.relu,
+    'padding': 'same'
 }
 
 MAXPOOL_LAYER_PARAMS = {
-    "pool_size": (2, 2),
-    "strides": 2,
-    "padding": 'same'
+    'pool_size': (2, 2),
+    'strides': 2,
+    'padding': 'same'
 }
 
-DENSE_LAYER_PARAMS = {
-    "units": 1024,
-    "activation": tf.nn.relu
+DENSE_LAYER_PARAMS_1 = {
+    'units': 512,
+    'activation': tf.nn.relu
 }
+
+DENSE_LAYER_PARAMS_2 = {
+    'units': 256,
+    'activation': tf.nn.relu
+}
+
+MODEL_CHECKPOINT_SAMPLING = 100
+ACCURACY_SAMPLING = 10
+CHECKPOINT_FILENAME = 'cnn.ckpt'
 
 
 def _parse_dataset_features(entry):
@@ -71,10 +95,12 @@ def plot_curve(curve, fig_name, fig_ext='.png'):
     plt.figure()
     plt.plot(range(1, measures_num + 1), curve)
     plt.axis([1, measures_num, 0, 1])
+    plt.ylabel('Accuracy')
     plt.savefig(fig, bbox_inches='tight')
 
     plt.figure()
     plt.plot(range(1, measures_num + 1), curve)
+    plt.ylabel('Accuracy')
     plt.savefig(fig_zoom, bbox_inches='tight')
 
 
@@ -95,33 +121,38 @@ def main(argv=None):
     global_step = tf.Variable(initial_value=0, name='global_step', trainable=False)
     X = tf.placeholder(tf.float32, [None, 32, 32, 3], name='input')
     y = tf.placeholder(tf.float32, [None, CIFAR10_CLASSES], name='labels')
-    keep_prob = tf.placeholder(tf.float32)
+    keep_prob_dense = tf.placeholder(tf.float32)
+    keep_prob_conv = tf.placeholder(tf.float32)
 
     ####################################################################################################################
 
     # convolutional layers
-    conv1 = tf.layers.conv2d(X, name='conv1', **CONV_LAYER_PARAMS)
-    maxpool1 = tf.layers.max_pooling2d(conv1, name='maxpool1', **MAXPOOL_LAYER_PARAMS)
+    conv1 = tf.layers.conv2d(X, name='conv1', **CONV_LAYER_PARAMS_1)
+    conv1_maxpool = tf.layers.max_pooling2d(conv1, name='conv1_maxpool', **MAXPOOL_LAYER_PARAMS)
+    conv1_dropout = tf.nn.dropout(conv1_maxpool, name='conv1_dropout', keep_prob=keep_prob_conv)
 
-    conv2 = tf.layers.conv2d(maxpool1, name='conv2', **CONV_LAYER_PARAMS)
-    maxpool2 = tf.layers.max_pooling2d(conv2, name='maxpool2', **MAXPOOL_LAYER_PARAMS)
+    conv2 = tf.layers.conv2d(conv1_dropout, name='conv2', **CONV_LAYER_PARAMS_2)
+    conv2_maxpool = tf.layers.max_pooling2d(conv2, name='conv2_maxpool', **MAXPOOL_LAYER_PARAMS)
 
-    conv3 = tf.layers.conv2d(maxpool2, name='conv3', **CONV_LAYER_PARAMS)
-    maxpool3 = tf.layers.max_pooling2d(conv3, name='maxpool3', **MAXPOOL_LAYER_PARAMS)
+    conv3 = tf.layers.conv2d(conv2_maxpool, name='conv3', **CONV_LAYER_PARAMS_2)
+    conv3_maxpool = tf.layers.max_pooling2d(conv3, name='conv3_maxpool', **MAXPOOL_LAYER_PARAMS)
+    conv3_dropout = tf.nn.dropout(conv3_maxpool, name='conv3_dropout', keep_prob=keep_prob_conv)
 
-    conv4 = tf.layers.conv2d(maxpool3, name='conv4', **CONV_LAYER_PARAMS)
-    maxpool4 = tf.layers.max_pooling2d(conv4, name='maxpool4', **MAXPOOL_LAYER_PARAMS)
+    conv4 = tf.layers.conv2d(conv3_dropout, name='conv4', **CONV_LAYER_PARAMS_3)
+    conv4_maxpool = tf.layers.max_pooling2d(conv4, name='conv4_maxpool', **MAXPOOL_LAYER_PARAMS)
 
-    conv5 = tf.layers.conv2d(maxpool4, name='conv5', **CONV_LAYER_PARAMS)
-    maxpool5 = tf.layers.max_pooling2d(conv5, name='maxpool5', **MAXPOOL_LAYER_PARAMS)
+    conv5 = tf.layers.conv2d(conv4_maxpool, name='conv5', **CONV_LAYER_PARAMS_3)
+    conv5_maxpool = tf.layers.max_pooling2d(conv5, name='conv5_maxpool', **MAXPOOL_LAYER_PARAMS)
+    conv5_dropout = tf.nn.dropout(conv5_maxpool, name='conv5_dropout', keep_prob=keep_prob_conv)
 
     # fully-connected layers
-    new_shape = [-1, maxpool5.shape[1] * maxpool5.shape[2] * maxpool5.shape[3]]
-    dense1 = tf.layers.dense(tf.reshape(maxpool5, new_shape), name='dense1', **DENSE_LAYER_PARAMS)
-    dense1_dropout = tf.nn.dropout(dense1, name='dense1_dropout', keep_prob=keep_prob)
+    dense1 = tf.layers.dense(
+        tf.reshape(conv5_dropout, [-1, conv5_dropout.shape[1] * conv5_dropout.shape[2] * conv5_dropout.shape[3]]),
+        name='dense1', **DENSE_LAYER_PARAMS_1)
+    dense1_dropout = tf.nn.dropout(dense1, name='dense1_dropout', keep_prob=keep_prob_dense)
 
-    dense2 = tf.layers.dense(dense1_dropout, name='dense2', **DENSE_LAYER_PARAMS)
-    dense2_dropout = tf.nn.dropout(dense2, name='dense2_dropout', keep_prob=keep_prob)
+    dense2 = tf.layers.dense(dense1_dropout, name='dense2', **DENSE_LAYER_PARAMS_2)
+    dense2_dropout = tf.nn.dropout(dense2, name='dense2_dropout', keep_prob=keep_prob_dense)
 
     output = tf.layers.dense(dense2_dropout, name='output', units=CIFAR10_CLASSES)
 
@@ -154,11 +185,13 @@ def main(argv=None):
                 X_batch, y_batch = sess.run(next_elem)
 
                 # train network
-                sess.run(train_step, feed_dict={X: X_batch, y: y_batch, keep_prob: KEEP_PROB})
+                sess.run(train_step, feed_dict={
+                    X: X_batch, y: y_batch, keep_prob_dense: KEEP_PROB_DENSE, keep_prob_conv: KEEP_PROB_CONV})
 
                 # compute accuracy
                 if epoch % ACCURACY_SAMPLING == 0 or epoch == EPOCHS - 1:
-                    epoch_accuracy = sess.run(accuracy, feed_dict={X: X_batch, y: y_batch, keep_prob: 1.0})
+                    epoch_accuracy = sess.run(accuracy, feed_dict={
+                        X: X_batch, y: y_batch, keep_prob_dense: 1.0, keep_prob_conv: 1.0})
                     training_accuracy_curve.append(epoch_accuracy)
                     print('accuracy: {}'.format(epoch_accuracy))
 
@@ -174,7 +207,6 @@ def main(argv=None):
         elapsed_time = (time.time() - start_time)
         training_time = timedelta(seconds=elapsed_time)
         print('training time:', training_time)
-        print('training accuracy curve:', training_accuracy_curve)
         plot_curve(training_accuracy_curve, fig_name=os.path.join(RESULTS_DIR, 'train_accuracy'))
 
         ################################################################################################################
@@ -185,22 +217,29 @@ def main(argv=None):
         iterator = test_dataset.make_one_shot_iterator()
         next_elem = iterator.get_next()
 
-        test_accuracy_collection = []
+        test_accuracy_curve = []
         try:
             while True:
                 X_test, y_test = sess.run(next_elem)
-                test_accuracy_collection.append(sess.run(accuracy, feed_dict={X: X_test, y: y_test, keep_prob: 1.0}))
+                test_accuracy = sess.run(accuracy, feed_dict={
+                    X: X_test, y: y_test, keep_prob_dense: 1.0, keep_prob_conv: 1.0})
+                test_accuracy_curve.append(test_accuracy)
         except tf.errors.OutOfRangeError:
             print('Test set has been consumed.')
 
-        test_accuracy = sess.run(tf.reduce_mean(tf.convert_to_tensor(test_accuracy_collection)))
-        print('test accuracy:', test_accuracy)
+        plot_curve(test_accuracy_curve, fig_name=os.path.join(RESULTS_DIR, 'test_accuracy'))
+        test_accuracy = sess.run(tf.reduce_mean(tf.convert_to_tensor(test_accuracy_curve)))
+        print('avg test accuracy:', test_accuracy)
+
+        ################################################################################################################
 
         # store results
-        results = dict()
-        results['training_time'] = training_time
-        results['training_accuracy_curve'] = training_accuracy_curve
-        results['test_accuracy'] = test_accuracy
+        results = {
+            'training_time': training_time,
+            'training_accuracy_curve': training_accuracy_curve,
+            'test_accuracy_curve': test_accuracy_curve,
+            'avg_test_accuracy': test_accuracy
+        }
         dump_results(results, os.path.join(RESULTS_DIR, 'results.pickle'))
 
 
